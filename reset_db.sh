a3d4e11bc74ed70232b8bb0a51772a6a3990fdc9
Thang Man <manvanthang@gmail.com>
2026-03-09 18:36:28 +0700

    Comment out reset django admin

--- reset_db.sh
+++ reset_db.sh
@@ -4,5 +4,5 @@ find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
 find . -path "*/migrations/*.pyc" -delete
 python manage.py makemigrations
 python manage.py migrate
-export DJANGO_SUPERUSER_PASSWORD="sandals-sick-there"
-python manage.py createsuperuser --noinput --username="admin" --email="admin@example.com" || true
+# export DJANGO_SUPERUSER_PASSWORD="sandals-sick-there"
+# python manage.py createsuperuser --noinput --username="admin" --email="admin@example.com" || true
